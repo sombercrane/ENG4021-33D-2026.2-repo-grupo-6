1ca1491b3efcac339b983c236399fded24d2b6bc
Eduardo <eduleitao43@gmail.com>
2026-09-03 18:24:28 -0300

    fazendo o merge

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 #Calculadora(turma 33D grupo 6)
-def calcula_subtração(x,y)
+def calcula_subtracao(x,y):
     return x - y
-def calcula_percentual(x,y)
-    return(x/100)*y
-print(calcula_subtracao(10, 3))
-print(calcula_percentual(20, 50))
-
+def calcula_percentual(x,y):
+    return(x/100)
