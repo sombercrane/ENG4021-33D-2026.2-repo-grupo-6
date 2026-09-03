9b163a24b8ec326597163303b1349431df62d32d
Eduardo <eduleitao43@gmail.com>
2026-09-03 14:43:13 -0300

    Criando funções de subtração e percentual

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("Eu sou fluminense")
+#Calculadora(turma 33D grupo 6)
+def calcula_subtração(x,y)
+    return x - y
+def calcula_percentual(x,y)
+    return(x/100)*y
+print(calcula_subtracao(10, 3))
+print(calcula_percentual(20, 50))
+
